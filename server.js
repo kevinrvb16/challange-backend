a71058df1e07c9c77037840debe10a7f9486d8a9
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2024-04-27 13:50:57 -0300

    created routes to access server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const nanoid = require('nanoid');
-const connectToDatabase = require('./db');
-const dotenv = require('dotenv');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { nanoid } from 'nanoid';
+import connectToDatabase from './database/index.cjs';
+import dotenv from 'dotenv';
+import {
+    createMessage,
+    getAllMessages,
+    getMessageById,
+    updateMessageById,
+    deleteMessageById,
+} from './database/model.cjs';
 dotenv.config();
 
 connectToDatabase();
@@ -17,8 +24,43 @@ let messages = [];
 app.post('/messages', (req, res) => {
     const { title, description } = req.body;
     const newMessage = { id: nanoid(), title, description };
-    messages.push(newMessage);
-    res.status(201).json(newMessage);
+    createMessage(newMessage).then((message) => {
+        res.status(201).json(message);
+    }).catch((error) => {
+        res.status(500).json({ message: error.message });
+    });
+});
+
+app.get('/messages', (req, res) => {
+    getAllMessages().then((messages) => {
+        res.status(200).json(messages);
+    }).catch((error) => {
+        res.status(500).json({ message: error.message });
+    });
+});
+
+app.get('/messages/:id', (req, res) => {
+    getMessageById(req.params.id).then((message) => {
+        res.status(200).json(message);
+    }).catch((error) => {
+        res.status(500).json({ message: error.message });
+    });
+});
+
+app.delete('/messages/:id', (req, res) => {
+    deleteMessageById(req.params.id).then(() => {
+        res.status(204).send();
+    }).catch((error) => {
+        res.status(500).json({ message: error.message });
+    });
+});
+
+app.put('/messages/:id', (req, res) => {
+    updateMessageById(req.params.id, req.body).then((message) => {
+        res.status(200).json(message);
+    }).catch((error) => {
+        res.status(500).json({ message: error.message });
+    });
 });
 
 app.listen(PORT, () => {
